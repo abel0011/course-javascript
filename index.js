96a85b57922fcffe2d9b035e897b1f72016e6c6c
abel Paz <abellpazz12@gmail.com>
2021-07-13 09:24:23 -0500

    Realizando asignacion de function ,luego de cargar html

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
-const btnadd = document.getElementById("add");
-
 //btnadd.addEventListener("click", () => {
 //  console.log("Hola como estas aprendiendo javascript");
 //});
+
+//DOMContentLoaded: cargar java scripit hasta que el html este cargado
+document.addEventListener("DOMContentLoaded", () => {
+  const title = document.getElementById("title");
+  const description = document.getElementById("description");
+  const btnadd = document.getElementById("add");
+  const table = document.getElementById("table");
+
+  //  btnadd.onclick = () => {
+  //    console.log(
+  //      "Title: ",
+  //      title.value,
+  //      "\n",
+  //      "Description: ",
+  //      description.value
+  //    );
+  //  };
+  //
+  function addTodo() {
+    if (title.value === "" || description.value === "") {
+      console.error("Title and Description are required");
+    } else {
+      console.log(
+        "El Titulo es => ",
+        title.value,
+        "\n",
+        "Description es =>",
+        description.value
+      );
+    }
+  }
+
+  btnadd.onclick = addTodo;
+
+  //  btnadd.addEventListener("click", () => {
+  //    console.log("El titulo es : ", title.value);
+  //  });
+});
